e914cd98699880310594709422be6856665a1a11
w-ingles <w-ingles@hotmail.com>
2024-08-04 13:55:28 -0300

    Incluindo rotas protegidas

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class CardapioController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      */
--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class RestauranteController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      */
